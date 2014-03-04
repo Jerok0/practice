34580b021c5adc5571fb5f3d970e1f5f242a3ea0
Jeroko <Jerok0@users.noreply.github.com>
2014-03-04 11:18:36 +0530

    yeop

--- tryhash.rb
+++
@@ -1,9 +0,0 @@
-myhash = {
-	name:      "Yadnyesh",
-	lname:      "Gokhale",
-	age:        17,
-	status:     "Learning ruby",
-	motivation: "varialbe, ballpark 12/13"
-}
-
-printf("%s %s\n", myhash[:name], myhash[:lname])
